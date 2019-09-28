a566773299d11871bc9d2ad72b4e8b32542f33be
Szymon Wojciechowski <infero93.ulx@gmail.com>
2019-09-28 21:59:02 +0200

    Live reload example

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var $ = require("jquery");
 var QRCode = require('qrcode')
 var canvas = document.getElementById('canvas')
  
-QRCode.toCanvas(canvas, 'sample text', function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-})
+
+$( "#qrvalue" ).keypress(function() {
+    QRCode.toCanvas(canvas, $(this).val(), function (error) {
+        if (error) console.error(error)
+        console.log('success!');
+      })
+});
+
